c3c46d87e8d5ebe3666e2fe86101780c5b8157a2
jovenntann <jovenntann@gmail.com>
2019-03-22 20:16:53 +0800

    Message to URL Encoding

--- sirvasms/sirvasmsapp/scripts/receiver.py
+++ sirvasms/sirvasmsapp/scripts/receiver.py
@@ -15,8 +15,7 @@ import urllib
 def goip_send(number,message,provider,goip):
  
     message = urllib.quote_plus(message)
-    print(message)
-
+    
     url = "http://localhost/goip/en/dosend.php?USERNAME=root&PASSWORD=root&smsprovider=" + str(provider) + "&goipname=" + goip + "&smsnum=" + number + "&method=2&Memo=" + str(message)
     reply = requests.post(url)
     messageid = re.search(r'messageid=(.*?)&USERNAME',reply.text).group(1)
