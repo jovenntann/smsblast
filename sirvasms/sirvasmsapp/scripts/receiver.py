34ca50ac975c412547fc42c33bab2bc540275112
jovenntann <jovenntann@gmail.com>
2019-03-22 17:18:14 +0800

    Forward Received SMS

--- sirvasms/sirvasmsapp/scripts/receiver.py
+++ sirvasms/sirvasmsapp/scripts/receiver.py
@@ -3,6 +3,39 @@ import time
 import datetime
 import re
 
+# GoIP API
+import requests
+import json
+import pprint
+import re
+
+def goip_send(number,message,goip):
+ 
+    if goip == 'GoIPD07':
+        provider = 3
+    elif provider == 'GoIPD08':
+        provider = 4
+
+    # Replace Provider
+    url = "http://localhost/goip/en/dosend.php?USERNAME=root&PASSWORD=root&smsprovider=" + str(provider) + "&goipname=" + goip + "&smsnum=" + number + "&method=2&Memo=" + message
+    reply = requests.post(url)
+    # print(reply.text)
+    messageid = re.search(r'messageid=(.*?)&USERNAME',reply.text).group(1)
+
+    send_url = "http://localhost/goip/en/resend.php?messageid=" + messageid + "&USERNAME=root&PASSWORD=root"
+    send_reply = requests.post(send_url)
+    # print(send_reply.text)
+
+    if 'errorstatus' in send_reply.text:
+        status = re.search(r'errorstatus:(.*)',send_reply.text).group(1)
+        status = 'Failed'
+    elif 'ok(' in send_reply.text:
+        status = 'Sent'
+    else:
+        status = 'Failed'
+
+    return {'messageid':messageid,'status':status}
+
 def insertSMS(date, from_number, to_number, message, MessageSid, status):
 
         from_number = from_number.replace('+639','09')
@@ -15,7 +48,7 @@ def insertSMS(date, from_number, to_number, message, MessageSid, status):
         cursor.execute(sql, ('AUTO', date, from_number, name, to_number, message, MessageSid, status))
         conn.commit()
         conn.close()
-
+        
 def updateStatus(_id,status):
   
         conn = mysql.connector.Connect(host='localhost',user='root',password='09106850351',database='goip')
@@ -63,6 +96,10 @@ while True:
 
                 insertSMS(date, srcnum, to_number, msg, _id, 'Received')
                 updateStatus(_id,1)
+
+                # Forward SMS
+                results = goip_send('09331707870',msg,'GoIPD07')
+                print(results)
 
 
 
